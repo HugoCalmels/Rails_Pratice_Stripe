6a940b3ca3352eee34206f8ded739a36ce59e2cd
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-12-03 02:38:55 +0100

    idk

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :products
-
+  resources :webhooks, only: [:create]
   resources :checkout, only: [:create]
   post "checkout/create", to: "checkout#create"
   root "products#index"
